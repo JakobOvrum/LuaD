de2830e1ad84428ae30fac2c985f1ba7db9f5af4
Jakob Ovrum <jakobovrum@gmail.com>
2010-08-22 17:10:13 +0900

    Tweaked a path in tree.js to point to the right image

--- candydoc/tree.js
+++ candydoc/tree.js
@@ -100,7 +100,7 @@ function TreeNode(text, iconSrc, hrefMode)
         if (name == "")
             name = "shim";
         
-        return "candydoc/img/tree/" + name + ".gif";
+        return "/LuaD/candydoc/img/tree/" + name + ".gif";
     }
     
     function setSrc(icon, src)
