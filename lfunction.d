b40b2fdab3d6543e33263b908b829d2f4db1a7f9
Jakob Ovrum <jakobovrum@gmail.com>
2010-08-23 16:59:19 +0900

    Documented LuaFunction a bit

--- lfunction.d
+++ lfunction.d
@@ -12,25 +12,61 @@ class LuaFunction : LuaObject
 		super(L, idx);
 	}
 	
+	/**
+	 * Same as call!(LuaObject[])(args);
+	 * Examples:
+	 -----------------------
+	lua.doString(`function f(...) return ... end`);
+	auto f = lua.get!LuaFunction("f");
+	
+	LuaObject[] ret = f(1.2, "hello!", true);
+	
+	assert(ret[0].to!double() == 1.2);
+	assert(ret[1].to!string() == "hello!");
+	assert(ret[2].to!bool());
+	 -----------------------
+	 */
 	LuaObject[] opCall(U...)(U args)
 	{
 		return call!(LuaObject[])(args);
 	}
 	
-	T call(T, U...)(U args)
+	/**
+	 * Call this function.
+	 * Params:
+	 *     T = expected return type.
+	 *     args = list of arguments.
+	 * Returns:
+	 *     Return value of type T, or nothing if T was unspecified.
+	 *     As a special case, a value of LuaObject[] for T will result
+	 *     all return values being collected and returned in a LuaObject[].
+	 * Examples:
+	 * ------------------
+	lua.doString(`function ask(question) return 42 end`);
+	auto ask = lua.get!LuaFunction("ask");
+	
+	auto answer = ask.call!int("What's the answer to life, the universe and everything?");
+	assert(answer == 42);
+	 * ------------------
+	 */
+	T call(T = void, U...)(U args)
 	{
 		push();
 		foreach(arg; args)
 			pushValue(state, arg);
 		
+		enum hasReturnValue = !is(T == void);
 		enum multiRet = is(T == LuaObject[]);
 		
-		lua_call(state, args.length, multiRet? LUA_MULTRET : 1);
+		lua_call(state, args.length, hasReturnValue? (multiRet? LUA_MULTRET : 1) : 0);
 		
-		static if(multiRet)
-			return getStack(state);
-		else
-			return popValue!T(state);
+		static if(hasReturnValue)
+		{
+			static if(multiRet)
+				return getStack(state);
+			else
+				return popValue!T(state);
+		}
 	}
 }
 
@@ -46,4 +82,6 @@ unittest
 	LuaObject[] ret = tostring(123);
 	assert(ret[0].to!string() == "123");
 	assert(tostring.call!string(123) == "123");
+	
+	tostring.call(321);
 }
