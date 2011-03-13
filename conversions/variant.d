79e29663d9578ed72f41eea58928af2aa24372f0
Jesse Phillips <Jesse.K.Phillips@gmail.com>
2011-03-13 13:47:43 -0700

    Fixed formatting (tabs)

--- conversions/variant.d
+++ conversions/variant.d
@@ -14,63 +14,62 @@ void pushVariant(T)(lua_State* L, ref T value) if (isVariant!T)
 			pushValue(L, value.get!Type);
 }
 
-T getVariant(T)(lua_State* L, int idx) if(isVariant!T)
+T getVariant(T)(lua_State* L, int idx) if (isVariant!T)
 {
 	T v;
 	foreach(Type; T.AllowedTypes)
 		if(isStackNativeType!Type(L, idx))
-            v = getValue!Type(L, idx);
+			v = getValue!Type(L, idx);
 
-    return v;
+	return v;
 }
 
 bool isAllowedType(T)(lua_State* L, int idx) {
 	foreach(Type; T.AllowedTypes)
-        if(isStackNativeType!Type(L, idx))
-            return true;
+		if(isStackNativeType!Type(L, idx))
+			return true;
 
-    return false;
+	return false;
 }
 
 
 bool isStackNativeType(T)(lua_State* L, int idx) {
-    auto luaT = lua_type(L, idx);
-    
-    if(lua_iscfunction(L, idx))
-        return isSomeFunction!T;
+	auto luaT = lua_type(L, idx);
+	
+	if(lua_iscfunction(L, idx))
+		return isSomeFunction!T;
 
-    if(luaT == LuaType.Function)
-        return isSomeFunction!T;
+	if(luaT == LuaType.Function)
+		return isSomeFunction!T;
 
-    if(luaT == LuaType.Table)
-        static if(is(T == class) || is(T == struct))
-            return true;
+	if(luaT == LuaType.Table)
+		static if(is(T == class) || is(T == struct))
+			return true;
 
-        else static if(isVariant!T)
-            return true;
+		else static if(isVariant!T)
+			return true;
 
-        else static if(isAssociativeArray!T)
-            return true;
+		else static if(isAssociativeArray!T)
+			return true;
 
-        else static if(isArray!T)
-            return true;
+		else static if(isArray!T)
+			return true;
 
-    if(luaT == LuaType.Boolean)
-        return is(T == bool);
+	if(luaT == LuaType.Boolean)
+		return is(T == bool);
 
-    if(luaT == LuaType.Number)
-        return isNumeric!T;
+	if(luaT == LuaType.Number)
+		return isNumeric!T;
 
-    if(luaT == LuaType.String)
-        return isSomeString!T;
+	if(luaT == LuaType.String)
+		return isSomeString!T;
 
-    return false;
-
+	return false;
 }
 
 template isVariant(T)
 {
-    enum isVariant = hasMember!(T,"AllowedTypes");
+	enum isVariant = hasMember!(T,"AllowedTypes");
 }
 
 
@@ -96,7 +95,7 @@ unittest
 //		for key, expected in pairs{i = 1, n = 2.3, s = "hello"} do 
 //			local value = struct[key]
 //			assert(value == expected, 
-//				("bad table pair: '%s' = '%s' (expected '%s')"):format(key, value, expected)
+//	("bad table pair: '%s' = '%s' (expected '%s')"):format(key, value, expected)
 //			)
 //		end
 //	`, __FILE__);
