717a9895cfdca9cef7bf174cf5760a6d113f590d
Jakob Ovrum <jakobovrum@gmail.com>
2012-04-19 10:32:18 +0900

    style fix for precompiled example (my fault)

--- example/precompiled/precompiled.d
+++ example/precompiled/precompiled.d
@@ -11,11 +11,11 @@ void main()
     LuaFunction func = lua.loadString( `return "Hello from precompiled Lua!"` );
 
     string fileName = "./ldump.luac";
-    auto f = new std.stream.File(fileName, FileMode.Out);
-    scope(exit) std.file.remove(fileName);
+    auto f = new std.stream.File( fileName, FileMode.Out );
+    scope(exit) std.file.remove( fileName );
     func.dump( data => f.writeBlock(data.ptr, data.length) == data.length );
     f.close();
 
     LuaObject[] ret = lua.doFile( fileName );
-    writeln(ret[0]);
+    writeln( ret[0] );
 }
