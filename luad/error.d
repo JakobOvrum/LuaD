76d66a38d945ef80e4fcea0989d7cc43397a6ed2
Jakob Ovrum <jakobovrum@gmail.com>
2012-11-07 22:08:53 +0900

    Some documentation formatting in luad.error

--- luad/error.d
+++ luad/error.d
@@ -3,13 +3,13 @@ module luad.error;
 /**
  * Thrown on Lua panic.
 
- * This exception is only thrown if there is no protected Lua call (e.g. pcall)
+ * This exception is only thrown if there is no protected Lua call (e.g. $(D pcall))
  * to handle the error. Whenever this documentation says a function throws
- * a LuaErrorException, no D exception is actually thrown if a Lua error handler is in place.
+ * a $(D LuaErrorException), no D exception is actually thrown if a Lua error handler is in place.
 
  * Additionally, exceptions thrown in D functions given to Lua are converted to
  * and propagated as Lua errors. You can subvert this by throwing an object not
- * deriving from Exception (i.e. derive from Error, Throwable, etc).
+ * deriving from $(D Exception) (i.e. derive from $(D Error), $(D Throwable), etc).
  */
 class LuaErrorException : Exception
 {
