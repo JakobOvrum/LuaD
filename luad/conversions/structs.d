409f91efab31ee9ea308f57ee5775bc52243725b
Manu Evans <turkeyman@gmail.com>
2014-08-18 03:12:12 +1000

    struct changed to use userdata blocks. Structs are handled by-ref in Lua code to match handling of regular tables.
    struct and class have uniform support for: methods, properties, member variable accessors.
    Refactor common code into helpers.d
    Added support for const function args.
    pushMethod enhanced to support struct's aswell.
    New alias syntax updates.

--- luad/conversions/structs.d
+++ luad/conversions/structs.d
@@ -1,71 +1,157 @@
 /**
-Internal module for pushing and getting _structs.
-
-A struct is treated as a table layout schema.
-Pushing a struct to Lua will create a table and fill it with key-value pairs - corresponding to struct fields - from the struct; the field name becomes the table key as a string.
-Struct methods are treated as if they were delegate fields pointing to the method.
+Internal module for pushing and getting structs.
+Structs are handled by-value across the LuaD API boundary, but internally managed by reference, with semantics equivalent to tables.
+Fields and properties are handled via a thin shim implemented in __index/__newindex. Methods are registered directly.
+mutable, const and immutable are all supported as expected. immutable structs will capture a direct reference to the D instance, and not be duplicated by LuaD.
 For an example, see the "Configuration File" example on the $(LINK2 $(REFERENCETOP),front page).
 */
 module luad.conversions.structs;
 
-import luad.c.all;
+import luad.conversions.helpers;
+import luad.conversions.functions;
 
+import luad.c.all;
 import luad.stack;
 
-private template isInternal(string field)
-{
-	enum isInternal = field.length >= 2 && field[0..2] == "__";
-}
+import core.memory;
 
-//TODO: ignore static fields, post-blits, destructors, etc?
-void pushStruct(T)(lua_State* L, ref T value) if (is(T == struct))
-{
-	lua_createtable(L, 0, value.tupleof.length);
+import std.traits;
+import std.conv;
 
-	foreach(field; __traits(allMembers, T))
+
+private void pushGetters(T)(lua_State* L)
+{
+	lua_newtable(L); // -2 is getters
+	lua_newtable(L); // -1 is methods
+
+	// populate getters
+	foreach(member; __traits(allMembers, T))
 	{
-		static if(!isInternal!field &&
-		          field != "this" &&
-		          field != "opAssign")
+		static if(!skipMember!(T, member) &&
+		          !isStaticMember!(T, member))
 		{
-			pushValue(L, field);
-
-			enum isMemberFunction = mixin("is(typeof(&value." ~ field ~ ") == delegate)");
-
-			static if(isMemberFunction)
-				pushValue(L, mixin("&value." ~ field));
-			else
-				pushValue(L, mixin("value." ~ field));
-
-			lua_settable(L, -3);
-		}
-	}
-}
-
-T getStruct(T)(lua_State* L, int idx) if(is(T == struct))
-{
-	T s;
-	fillStruct(L, idx, s);
-	return s;
-}
-
-void fillStruct(T)(lua_State* L, int idx, ref T s) if(is(T == struct))
-{
-	foreach(field; __traits(allMembers, T))
-	{
-		static if(field != "this" && !isInternal!(field))
-		{
-			static if(__traits(getOverloads, T, field).length == 0)
+			static if(isMemberFunction!(T, member) && !isProperty!(T, member))
 			{
-				lua_getfield(L, idx, field.ptr);
-				if(lua_isnil(L, -1) == 0) {
-					mixin("s." ~ field ~
-					  " = popValue!(typeof(s." ~ field ~ "))(L);");
-				} else
-					lua_pop(L, 1);
+				static if(canCall!(T, member))
+				{
+					pushMethod!(T, member)(L);
+					lua_setfield(L, -2, member.ptr);
+				}
+			}
+			else static if(canRead!(T, member)) // TODO: move into the getter for inaccessable fields (...and throw a useful error messasge)
+			{
+				pushGetter!(T, member)(L);
+				lua_setfield(L, -3, member.ptr);
 			}
 		}
 	}
+
+	lua_pushcclosure(L, &index, 2);
+}
+
+private void pushSetters(T)(lua_State* L)
+{
+	lua_newtable(L);
+
+	// populate setters
+	foreach(member; __traits(allMembers, T))
+	{
+		static if(!skipMember!(T, member) &&
+		          !isStaticMember!(T, member) &&
+		          canWrite!(T, member)) // TODO: move into the setter for readonly fields
+		{
+			static if(!isMemberFunction!(T, member) || isProperty!(T, member))
+			{
+				pushSetter!(T, member)(L);
+				lua_setfield(L, -2, member.ptr);
+			}
+		}
+	}
+
+	lua_pushcclosure(L, &newIndex, 1);
+}
+
+private void pushMeta(T)(lua_State* L)
+{
+	if(luaL_newmetatable(L, T.mangleof.ptr) == 0)
+		return;
+
+	pushValue(L, T.stringof);
+	lua_setfield(L, -2, "__dtype");
+
+	// TODO: mangled names can get REALLY long in D, it might be nicer to store a hash instead?
+	pushValue(L, T.mangleof);
+	lua_setfield(L, -2, "__dmangle");
+
+	lua_pushcfunction(L, &userdataCleaner);
+	lua_setfield(L, -2, "__gc");
+
+	pushGetters!T(L);
+	lua_setfield(L, -2, "__index");
+	pushSetters!T(L);
+	lua_setfield(L, -2, "__newindex");
+
+	static if(__traits(hasMember, T, "toString"))
+	{
+		pushMethod!(T, "toString")(L);
+		lua_setfield(L, -2, "__tostring");
+	}
+
+	static if(__traits(hasMember, T, "opEquals"))
+	{
+		pushMethod!(T, "opEquals")(L);
+		lua_setfield(L, -2, "__eq");
+	}
+	// TODO: __lt,__le (wrap opCmp)
+
+	// TODO: operators, etc...
+
+	lua_pushvalue(L, -1);
+	lua_setfield(L, -2, "__metatable");
+}
+
+void pushStruct(T)(lua_State* L, ref T value) if (is(T == struct))
+{
+	// if T is immutable, we can capture a reference, otherwise we need to take a copy
+	static if(is(T == immutable)) // TODO: verify that this is actually okay?
+	{
+		auto udata = cast(Ref!T*)lua_newuserdata(L, Ref!T.sizeof);
+		*udata = Ref!T(value);
+	}
+	else
+	{
+		Ref!T* udata = cast(Ref!T*)lua_newuserdata(L, Ref!T.sizeof);
+		// TODO: we should try and call the postblit here maybe...?
+//		T* copy = new T(value);
+//		T* copy = std.conv.emplace(cast(T*)GC.malloc(T.sizeof), value);
+		Unqual!T* copy = cast(Unqual!T*)GC.malloc(T.sizeof);
+		*copy = value;
+		*udata = Ref!T(*copy);
+	}
+
+	GC.addRoot(udata);
+
+	pushMeta!T(L);
+	lua_setmetatable(L, -2);
+}
+
+void pushStruct(R : Ref!T, T)(lua_State* L, R value) if (is(T == struct))
+{
+	auto udata = cast(Ref!T*)lua_newuserdata(L, Ref!T.sizeof);
+	*udata = Ref!T(value);
+
+	GC.addRoot(udata);
+
+	pushMeta!T(L);
+	lua_setmetatable(L, -2);
+}
+
+ref T getStruct(T)(lua_State* L, int idx) if(is(T == struct))
+{
+	verifyType!T(L, idx);
+
+	Ref!T* udata = cast(Ref!T*)lua_touserdata(L, idx);
+	return *udata;
 }
 
 version(unittest)
@@ -73,12 +159,33 @@ version(unittest)
 	import luad.base;
 	struct S
 	{
+		struct C
+		{
+			int i;
+		}
+
 		LuaObject o;
 		int i;
 		double n;
 		string s;
 
-		string f(){ return "foobar"; }
+		enum e = "enum";
+
+		C c;
+
+		string f() { return "foobar"; }
+
+		@property string p() { return prop; }
+		@property void p(string v) { prop = v; }
+
+		@property inout(C) io() inout { return c; }
+		@property void io(C v) { return c = v; }
+
+		@property ref C r() { return c; }
+		@property void r(ref C v) { return c = v; }
+
+	protected:
+		string prop = "getter";
 	}
 }
 
@@ -93,45 +200,82 @@ unittest
 	pushValue(L, "test");
 	auto obj = popValue!LuaObject(L);
 
-	pushValue(L, S(obj, 1, 2.3, "hello"));
-	assert(lua_istable(L, -1));
+	auto s = S(obj, 1, 2.3, "hello", S.I(10));
+	pushValue(L, s);
+	assert(lua_isuserdata(L, -1));
 	lua_setglobal(L, "struct");
 
 	unittest_lua(L, `
-		for key, expected in pairs{i = 1, n = 2.3, s = "hello"} do
+		for key, expected in pairs{i = 1, n = 2.3, s = "hello", e = "enum", p = "getter"} do
 			local value = struct[key]
 			assert(value == expected,
 				("bad table pair: '%s' = '%s' (expected '%s')"):format(key, value, expected)
 			)
 		end
 
-		assert(struct.f() == "foobar")
+		assert(struct:f() == "foobar")
+		assert(struct.c.i == 10)
+
+		-- test member struct
+		struct.c.i = 20
+		assert(struct.c.i == 20)
+
+		-- test property, return by value
+		struct.io.i = 30
+		assert(struct.io.i == 20)
+		local l = struct.io
+		l.i = 30
+		struct.io = l
+		assert(struct.io.i == 30)
+
+		-- test property, return by ref
+		struct.r.i = 40
+		assert(struct.r.i == 40)
+		l.i = 50
+		struct.r = l
+		assert(struct.r.i == 50)
+
+		-- set some values to return to the D code
+		struct.i = 2
+		struct.n = 4.6
+		struct.s = "world"
+		struct.c.i = 100
+		struct.p = "setter"
 	`);
 
 	lua_getglobal(L, "struct");
-	S s = getStruct!S(L, -1);
+	s = getValue!S(L, -1);
 
 	assert(s.o == obj);
-	assert(s.i == 1);
-	assert(s.n == 2.3);
-	assert(s.s == "hello");
+	assert(s.i == 2);
+	assert(s.n == 4.6);
+	assert(s.s == "world");
+	assert(s.i.i == 100);
+	assert(s.p == "setter");
 
 	lua_pop(L, 1);
 
-	struct S2
-	{
-		string a, b;
-	}
+/+
+	// TODO: test the type interface
 
+	// test constructor works
 	unittest_lua(L, `
-		incompleteStruct = {a = "foo"}
+		struct = S("test", 2, 4.6, "world")
 	`);
 
-	lua_getglobal(L, "incompleteStruct");
-	S2 s2 = getStruct!S2(L, -1);
+	lua_getglobal(L, "struct");
+	s = getValue!S(L, -1);
 
-	assert(s2.a == "foo");
-	assert(s2.b == null);
+	assert(s.o.to!string == "test");
+	assert(s.i == 2);
+	assert(s.n == 4.6);
+	assert(s.s == "world");
 
 	lua_pop(L, 1);
+
+	// test assigning member structs
+
+	// test static variables work
+	// test static methods work
++/
 }
--- luad/stack.d
+++ luad/stack.d
@@ -73,6 +73,7 @@ import luad.conversions.assocarrays;
 import luad.conversions.classes;
 import luad.conversions.enums;
 import luad.conversions.variant;
+import luad.conversions.helpers;
 
 /**
  * Push a value of any type to the stack.
@@ -80,7 +81,7 @@ import luad.conversions.variant;
  *	 L = stack to push to
  *	 value = value to push
  */
-void pushValue(T)(lua_State* L, T value)
+void pushValue(T)(lua_State* L, T value) if(!isUserStruct!T)
 {
 	static if(is(T : LuaObject))
 		value.push();
@@ -124,7 +125,7 @@ void pushValue(T)(lua_State* L, T value)
 	else static if(isArray!T)
 		pushArray(L, value);
 
-	else static if(is(T == struct))
+	else static if(is(T == Ref!S, S) && isUserStruct!S)
 		pushStruct(L, value);
 
 	// luaCFunction's are directly pushed
@@ -144,6 +145,18 @@ void pushValue(T)(lua_State* L, T value)
 	}
 	else
 		static assert(false, "Unsupported type `" ~ T.stringof ~ "` in stack push operation");
+}
+
+void pushValue(T)(lua_State* L, ref T value)  if(isUserStruct!T)
+{
+	static if(isArray!T)
+		pushArray(L, value);
+	else static if(is(T == struct))
+		pushStruct(L, value);
+	else
+	{
+		static assert(false, "Shouldn't be here! `" ~ T.stringof ~ "` should be handled by the other overload.");
+	}
 }
 
 template isVoidArray(T)
@@ -179,10 +192,10 @@ template luaTypeOf(T)
 	else static if(isSomeFunction!T || is(T == LuaFunction))
 		enum luaTypeOf = LUA_TFUNCTION;
 
-	else static if(isArray!T || isAssociativeArray!T || is(T == struct) || is(T == LuaTable))
+	else static if(isArray!T || isAssociativeArray!T || is(T == LuaTable))
 		enum luaTypeOf = LUA_TTABLE;
 
-	else static if(is(T : Object))
+	else static if(is(T : const(Object)) || is(T == struct))
 		enum luaTypeOf = LUA_TUSERDATA;
 
 	else
@@ -209,7 +222,7 @@ private void argumentTypeMismatch(lua_State* L, int idx, int expectedType)
  *	 L = stack to get from
  *	 idx = value stack index
  */
-T getValue(T, alias typeMismatchHandler = defaultTypeMismatch)(lua_State* L, int idx)
+T getValue(T, alias typeMismatchHandler = defaultTypeMismatch)(lua_State* L, int idx) if(!isUserStruct!T)
 {
 	debug //ensure unchanged stack
 	{
@@ -230,7 +243,7 @@ T getValue(T, alias typeMismatchHandler = defaultTypeMismatch)(lua_State* L, int
 		enum expectedType = luaTypeOf!T;
 
 		//if a class reference, return null for nil values
-		static if(is(T : Object))
+		static if(is(T : const(Object)))
 		{
 			if(type == LuaType.Nil)
 				return null;
@@ -298,13 +311,11 @@ T getValue(T, alias typeMismatchHandler = defaultTypeMismatch)(lua_State* L, int
 
 		return getVariant!T(L, idx);
 	}
-	else static if(is(T == struct))
-		return getStruct!T(L, idx);
 
 	else static if(isSomeFunction!T)
 		return getFunction!T(L, idx);
 
-	else static if(is(T : Object))
+	else static if(is(T : const(Object)))
 		return getClassInstance!T(L, idx);
 
 	else
@@ -313,11 +324,49 @@ T getValue(T, alias typeMismatchHandler = defaultTypeMismatch)(lua_State* L, int
 	}
 }
 
+// we need an overload that handles struct and static arrays (which need to return by ref)
+ref T getValue(T, alias typeMismatchHandler = defaultTypeMismatch)(lua_State* L, int idx) if(isUserStruct!T)
+{
+	debug //ensure unchanged stack
+	{
+		int _top = lua_gettop(L);
+		scope(success) assert(lua_gettop(L) == _top);
+	}
+
+	// TODO: confirm that we need this in this overload...?
+	static if(!is(T == LuaObject) && !is(T == LuaDynamic) && !isVariant!T)
+	{
+		int type = lua_type(L, idx);
+		enum expectedType = luaTypeOf!T;
+
+		//if a class reference, return null for nil values
+		static if(is(T : const(Object)))
+		{
+			if(type == LuaType.Nil)
+				return null;
+		}
+
+		if(type != expectedType)
+			typeMismatchHandler(L, idx, expectedType);
+	}
+
+	static if(isArray!T)
+		return getArray!T(L, idx);
+
+	else static if(is(T == struct))
+		return getStruct!T(L, idx);
+
+	else
+	{
+		static assert(false, "Shouldn't be here! `" ~ T.stringof ~ "` should be handled by the other overload.");
+	}
+}
+
 /**
  * Same as calling getValue!(T, typeMismatchHandler)(L, -1), then popping one value from the stack.
  * See_Also: $(MREF getValue)
  */
-T popValue(T, alias typeMismatchHandler = defaultTypeMismatch)(lua_State* L)
+auto ref popValue(T, alias typeMismatchHandler = defaultTypeMismatch)(lua_State* L)
 {
 	scope(success) lua_pop(L, 1);
 	return getValue!(T, typeMismatchHandler)(L, -1);
@@ -384,7 +433,13 @@ auto getArgument(T, int narg)(lua_State* L, int idx)
 		return cstr[0 .. len];
 	}
 	else
-		return getValue!(Arg, argumentTypeMismatch)(L, idx);
+	{
+		// TODO: make an overload to handle struct and static array, and remove this Ref! hack?
+		static if(isUserStruct!Arg) // user struct's need to return wrapped in a Ref
+			return Ref!Arg(getValue!(Arg, argumentTypeMismatch)(L, idx));
+		else
+			return getValue!(Arg, argumentTypeMismatch)(L, idx);
+	}
 }
 
 template isVariableReturnType(T : LuaVariableReturn!U, U)
@@ -487,7 +542,7 @@ int pushReturnValues(T)(lua_State* L, T value)
 		pushTuple(L, value);
 		return cast(int)T.Types.length;
 	}
-	else static if(isStaticArray!T)
+	else static if(isStaticArray!T) // TODO: remove this special case when we fix pushValue for static arrays
 	{
 		pushStaticArray(L, value);
 		return cast(int)value.length;
